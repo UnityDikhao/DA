3a184e35bd41f5e17fea156e8e40026a8caaa3e8
UnityDikhao <166284991+UnityDikhao@users.noreply.github.com>
2024-04-15 13:35:59 +0530

    Update S03.py

--- S03.py
+++ S03.py
@@ -1,9 +1,11 @@
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 import numpy as np
+
 
 np.random.seed(42)
 
@@ -17,17 +19,21 @@ data = {
 
 user_df = pd.DataFrame(data)
 
-
 X = user_df[['Age', 'EstimatedSalary']]
 y = user_df['Purchased']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
 model = LogisticRegression(random_state=42)
 
-model.fit(X_train, y_train)
+model.fit(X_train_scaled, y_train)
 
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
 accuracy = accuracy_score(y_test, y_pred)
 conf_matrix = confusion_matrix(y_test, y_pred)
